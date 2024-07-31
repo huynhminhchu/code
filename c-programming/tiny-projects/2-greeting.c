0dff6bfbc06ced811af4d053656696912eaaaf80
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-31 07:02:39 +0000

    Scripted auto-commit on change (2024-07-31 07:02:39) by gitwatch.sh

--- c-programming/tiny-projects/2-greeting.c
+++ c-programming/tiny-projects/2-greeting.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <time.h>
 
-int main() {
-  printf("Hello, Chu!\n");
+int main(int argc, char *argv[]) {
+
+  time_t now;
+  time(&now);
+  struct tm *clock;
+  clock = localtime(&now);
+  int hour = clock->tm_hour;
+  char *name = "Chu";
+
+  printf("Good ");
+  if (hour + 7 < 12) {
+    printf("morning, ");
+  } else if (hour + 7 >= 12 && hour + 7 < 17) {
+    printf("afternoon, ");
+  } else {
+    printf("evening, ");
+  }
+  if (argv[1] != NULL)
+    name = argv[1];
+  printf("%s!!!\n", name);
+  printf("It's %s", ctime(&now));
   return 0;
 }

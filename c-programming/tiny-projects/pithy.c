f2fa7f87fe7efcce3a594049488d6c68aa39fd38
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-31 07:48:42 +0000

    Scripted auto-commit on change (2024-07-31 07:48:42) by gitwatch.sh

--- c-programming/tiny-projects/pithy.c
+++ c-programming/tiny-projects/pithy.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BSIZE 256
 
 int main() {
-  const char filename[] = "pithy.next";
+  const char filename[] = "pithy.txt";
   FILE *fp;
   char buffer[BSIZE];
   char *r;
+  char *entry; // allocated based on the size of the line read from the file
+  // copy bfrom buffer to entry
 
   fp = fopen(filename, "r");
   if (fp == NULL) {
@@ -20,6 +23,7 @@ int main() {
     if (r == NULL)
       break;
     printf("%s", buffer);
+    printf("Size of buffer: %ld\n", strlen(buffer));
   }
   fclose(fp);
 }

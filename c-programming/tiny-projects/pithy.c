561bee6ab6db012248942f62f6e2fb2e26bb7676
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-31 08:14:37 +0000

    Scripted auto-commit on change (2024-07-31 08:14:37) by gitwatch.sh

--- c-programming/tiny-projects/pithy.c
+++ c-programming/tiny-projects/pithy.c
@@ -18,14 +18,32 @@ int main() {
     exit(1);
   }
 
+  int items, x;
+  char **list_base;
+
+  items = 0;
+  list_base = (char **)malloc(sizeof(char *) * 100);
+  if (list_base == NULL) {
+    fprintf(stderr, "Unable to allocate memory\n");
+    exit(1);
+  }
+
   while (!feof(fp)) {
     r = fgets(buffer, BSIZE, fp);
     if (r == NULL)
       break;
 
     entry = (char *)malloc(sizeof(char) * strlen(buffer) + 1);
+    if (entry == NULL) {
+      fprintf(stderr, "Unable to allocate memory\n");
+      exit(1);
+    }
     strcpy(entry, buffer);
-    printf("Entry: %s\n", entry);
+    *(list_base + items) = entry;
+    /* printf("d: %s", items, entry); */
+    items++;
   }
+  for (int i = 0; i < items; i++)
+    printf("%s", *(list_base + i));
   fclose(fp);
 }

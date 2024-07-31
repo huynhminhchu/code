7eff1932e89ab3e6989e018aee9779adad45c474
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-31 06:51:11 +0000

    Scripted auto-commit on change (2024-07-31 06:51:11) by gitwatch.sh

--- c-programming/tiny-projects/2-time.c
+++ c-programming/tiny-projects/2-time.c
@@ -10,5 +10,12 @@ int main() {
   // The number of seconds of tick-tocking since 1970
   printf("%s", ctime(&now));
 
+  struct tm *clock;
+  clock = localtime(&now);
+  puts("Time details:");
+  printf(" Day of the year: %d\n", clock->tm_yday);
+  printf(" Day of the week: %d\n", clock->tm_wday);
+  printf("            Year: %d\n", clock->tm_year);
+
   return 0;
 }

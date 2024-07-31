1a0e31a1846b25498ce0451d92b26f4040dabab3
Chu Huynh <huynhminhchu1999@gmail.com>
2024-07-31 06:53:19 +0000

    Scripted auto-commit on change (2024-07-31 06:53:19) by gitwatch.sh

--- c-programming/tiny-projects/2-time.c
+++ c-programming/tiny-projects/2-time.c
@@ -15,7 +15,12 @@ int main() {
   puts("Time details:");
   printf(" Day of the year: %d\n", clock->tm_yday);
   printf(" Day of the week: %d\n", clock->tm_wday);
-  printf("            Year: %d\n", clock->tm_year);
+  printf("            Year: %d\n", clock->tm_year + 1900);
+  printf("            Month: %d\n", clock->tm_mon + 1);
+  printf(" Day of the month: %d\n", clock->tm_mday);
+  printf("            Hour: %d\n", clock->tm_hour);
+  printf("            Minute: %d\n", clock->tm_min);
+  printf("            Second: %d\n", clock->tm_sec);
 
   return 0;
 }

e22a1e1bdf131c612e557cbc4d2ca0ad415b2047
Chu Huynh <huynhminhchu1999@gmail.com>
2024-08-02 07:34:47 +0000

    Scripted auto-commit on change (2024-08-02 07:34:47) by gitwatch.sh

--- c-programming/tiny-projects/3-nato-translator.c
+++ c-programming/tiny-projects/3-nato-translator.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Input: Howdy */
+/* Output: Hotel Oscar Whiskey Delta Yankee */
+
+/* Input 2: Hello, World! */
+/*   Output 2: Hotel Echo Lima Lima Oscar Whiskey Osca Romeo Lima Delta */
+
 int main(int argc, char *argv[]) {
   char *nato[] = {
       "Alfa",   "Bravo",   "Charlie", "Delta",  "Echo",   "Foxtrot", "Golf",
       "Hotel",  "India",   "Juliett", "Kilo",   "Lima",   "Mike",    "November",
       "Oscar",  "Papa",    "Quebec",  "Romeo",  "Sierra", "Tango",   "Uniform",
       "Victor", "Whiskey", "Xray",    "Yankee", "Zulu"};
+
+  int length = sizeof(nato) / sizeof(char *);
+
+  printf("Length: %d\n", length);
+  for (int i = 0; i < length; i++) {
+    printf("Nato code [%d]: %s\n", i + 1, nato[i]);
+  }
+  return 0;
 }

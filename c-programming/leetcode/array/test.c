120a3071d8a04e2c3ae87b80b3d491cdf0e092e3
Chu Huynh <huynhminhchu1999@gmail.com>
2024-09-10 00:20:25 +0000

    Scripted auto-commit on change (2024-09-10 00:20:25) by gitwatch.sh

--- c-programming/leetcode/array/test.c
+++ c-programming/leetcode/array/test.c
@@ -4,23 +4,8 @@
 
 int removeDuplicates(int* nums, int numsSize) {
     
-    int *a = malloc(numsSize * sizeof(int));
-    a[0] = nums[0];
-    printf("Hello World.\n");
-    if (numsSize == 1){
-        return 1;
-    }
-    printf("Hello World.\n");
-    int j = 1;
-    for (int i = 1; i < numsSize; i++){
-        if (nums[i] != nums[i-1]) {
-            a[j] = nums[i];
-            printf("nums[i]=%d\n", nums[i]);
-            printf("A[j]=%d\n", a[j]);
-            j++;
-        }
-    }
-    return j;
+    nums = realloc(nums, sizeof(int) * 100);
+    nums[numsSize] = 100;
 }
 
 int main()

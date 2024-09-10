4831de66afd14d733d549da348f0317f437a6b8c
Chu Huynh <huynhminhchu1999@gmail.com>
2024-09-10 03:29:54 +0000

    Scripted auto-commit on change (2024-09-10 03:29:54) by gitwatch.sh

--- c-programming/leetcode/array/test.c
+++ c-programming/leetcode/array/test.c
@@ -14,18 +14,21 @@ int removeDuplicates(int* nums, int numsSize) {
             break;
         }
     }
-    printf("j=%d\n", j);
+    // printf("j=%d\n", j);
 
     for (int i = 1; i < numsSize; i++){
         // if (j + 1 > numsSize){
         //     printf("break.\n");
         //     break;
         // }
-        count++;
+        
         if (nums[i] <= nums[i-1]) {
             nums[i] = nums[j];
-            if (nums[i] == nums[numsSize - 1])
+            printf("yes we made changed heres.\n");
+            if (nums[i] == nums[numsSize - 1]){
                 break;
+            }
+                
             for (int k = j + 1; k < numsSize; k ++){
                 if (nums[k] > nums[j]){
                     j = k;
@@ -33,13 +36,13 @@ int removeDuplicates(int* nums, int numsSize) {
                 }
                     
             }
-            printf("k=%d after the loop.\n", j);
+            printf("j=%d after the loop.\n", j);
         }
-
+        count++;
     }
-    for (int i  = 0; i < numsSize - 1; i++){
-        printf("%d\n", nums[i]);
-    }
+    // for (int i  = 0; i < numsSize; i++){
+    //     printf("Number: %d\n", nums[i]);
+    // }
     return count;
 }
 
@@ -48,9 +51,10 @@ int main()
     int a[10] = {-1,-1,1,1,2,2,2,3,3,4};
     int b[] = {1,1};
     // int a[3] = {1,1,2};
-    // int k = removeDuplicates(a, 3);
-    // int k = removeDuplicates(a, 10);
+    int k = removeDuplicates(a, 10);
     int k2= removeDuplicates(b, 2);
- //   printf("k=%d\n", k);
+    printf("k=%d\n", k);
     printf("k2=%d\n", k2);
+
+    
 }
